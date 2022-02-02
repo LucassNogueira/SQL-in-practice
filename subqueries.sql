06eecb057feb41c93a9bc6bcea7337ec9c8b0904
LucassNogueira <simplylucasjason@gmail.com>
2022-02-02 16:26:01 -0600

    this makes sense

--- subqueries.sql
+++ subqueries.sql
@@ -15,3 +15,8 @@ WHERE playlist_id IN (SELECT playlist_id
 WHERE track_id IN (SELECT track_id
                       FROM playlist_track
                       WHERE playlist_id = 5);
+
+                      SELECT * FROM track
+WHERE genre_id IN (SELECT genre_id
+                  FROM genre
+                  WHERE name = 'Comedy');
